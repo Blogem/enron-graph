b95b6d64cbc2c1983ea342184092aa99aaf4e48c
Blogem <>
2026-01-28 23:06:59 +0100

    Fix wails compilation

--- cmd/explorer/app_test_helper.go
+++ cmd/explorer/app_test_helper.go
@@ -6,21 +6,22 @@ import (
 	"github.com/Blogem/enron-graph/ent"
 	"github.com/Blogem/enron-graph/internal/chat"
 	"github.com/Blogem/enron-graph/internal/explorer"
+	"github.com/Blogem/enron-graph/pkg/llm"
 	"github.com/Blogem/enron-graph/pkg/utils"
 )
 
 // NewAppWithStub creates a new App with a stub LLM client for testing
-func NewAppWithStub(client *ent.Client, db *sql.DB, cfg *utils.Config) *App {
+func NewAppWithStub(client *ent.Client, db *sql.DB, cfg *utils.Config, llmClient llm.Client) *App {
 	// Create chat dependencies with stub
-	llmClient := chat.NewStubLLMClient()
-	chatHandler := chat.NewHandler(llmClient, nil)
+	llmClientChat := chat.NewStubLLMClient()
+	chatHandler := chat.NewHandler(llmClientChat, nil)
 	chatContext := chat.NewContext()
 
 	return &App{
 		client:        client,
 		config:        cfg,
 		schemaService: explorer.NewSchemaService(client, db),
-		graphService:  explorer.NewGraphService(client, db),
+		graphService:  explorer.NewGraphService(client, db, llmClient),
 		chatHandler:   chatHandler,
 		chatContext:   chatContext,
 	}
